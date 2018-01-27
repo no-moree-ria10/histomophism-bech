5c0904f35bf03e1d8a98382d0c76562e0f3b73b1
ria10 <otatakuma0714@gmail.com>
2018-01-27 17:26:40 +0900

    ad fib[2000] bench

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -17,26 +17,24 @@ main = defaultMain [
                       , bench "9"  $ whnf solveFib 9
                       , bench "11" $ whnf solveFib 11
                       , bench "20" $ whnf solveFib 20
-                      , bench "50" $ whnf solveFib 50
-                      , bench "70" $ whnf solveFib 70
-                      , bench "90" $ whnf solveFib 90
+                      , bench "200" $ whnf solveFib 200
+                      , bench "2000" $ whnf solveFib 2000
                       ]
   , bgroup "sovleFib'" [ bench "1"  $ whnf solveFib' 1
                       , bench "5"  $ whnf solveFib' 5
                       , bench "9"  $ whnf solveFib' 9
                       , bench "11" $ whnf solveFib' 11
                       , bench "20" $ whnf solveFib' 20
-                      , bench "50" $ whnf solveFib' 50
-                      , bench "70" $ whnf solveFib' 70
-                      , bench "90" $ whnf solveFib' 90
+                      , bench "200" $ whnf solveFib' 200
+                      , bench "2000" $ whnf solveFib' 2000
                ]
-  , bgroup "naiveFib" [ bench "1"  $ whnf naiveFib 1
-                      , bench "5"  $ whnf naiveFib 5
-                      , bench "9"  $ whnf naiveFib 9
-                      , bench "11" $ whnf naiveFib 11
-                      -- , bench "12" $ whnf naiveFib 12
-                      -- , bench "13" $ whnf naiveFib 13
-                      -- , bench "14" $ whnf naiveFib 14
-                      -- , bench "15" $ whnf naiveFib 15
-                      ]
+  -- , bgroup "naiveFib" [ bench "1"  $ whnf naiveFib 1
+  --                     , bench "5"  $ whnf naiveFib 5
+  --                     , bench "9"  $ whnf naiveFib 9
+  --                     , bench "11" $ whnf naiveFib 11
+  --                     -- , bench "12" $ whnf naiveFib 12
+  --                     -- , bench "13" $ whnf naiveFib 13
+  --                     -- , bench "14" $ whnf naiveFib 14
+  --                     -- , bench "15" $ whnf naiveFib 15
+  --                     ]
   ]
