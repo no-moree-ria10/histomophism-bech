2a1b5c2f4ecf5d29a711d7b90ec636e66466efc4
ria10 <otatakuma0714@gmail.com>
2018-01-27 14:06:02 +0900

    ad histo(fib)

---
+++ src/Main.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE DeriveFunctor #-}
+module Main where
+
+import Control.Arrow
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+
+
+
+
+
+
+data Attr f a = Attr
+              { attribute :: a
+              , hole      :: f (Attr f a)
+              }
+
+
+type CVAlgebra f a = f (Attr f a) -> a
+
+histo :: Functor f => CVAlgebra f a -> Term f -> a
+
+histo h  = worker >>> attribute 
+  where
+    worker = out >>> fmap worker >>> (h &&& id) >>> mkAttr
+    mkAttr (a, b) = Attr a b
+
+data ProgressInt i = Finish
+                   | I Int i
+                   deriving(Eq,Ord,Functor)
+
+newtype Term f = In { out :: f (Term f) }
+
+n2one :: Int ->  Term ProgressInt
+n2one 0 = In $ Finish  
+n2one i = In $ I i $  n2one (i-1) 
+
+fibCVAlgebra :: CVAlgebra ProgressInt Int 
+fibCVAlgebra Finish = 0
+fibCVAlgebra (I 1 attr) = 1
+fibCVAlgebra (I 2 attr) = 1
+fibCVAlgebra (I i attr) = attribute attr + (attribute .wkr. hole) attr
+  where
+    wkr ( I idx attr')  = attr'
+
+solveFib i = histo fibCVAlgebra (n2one i)
